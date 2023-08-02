021eb4ce7e8507bd0a52251cfe32e727c7b067fb
henryhyde11 <serestrepov@gmail.com>
2023-08-02 15:14:40 -0500

    Login updated: structure and font

--- src/components/LoginComponent.jsx
+++ src/components/LoginComponent.jsx
@@ -1,30 +1,69 @@
+import { FcGoogle } from "react-icons/fc";
+import { AiFillFacebook } from "react-icons/ai";
+
 export function LoginComponent() {
   return (
-    <main className="flex justify-center p-5 py-9 bg-slate-200">
-      <form className="flex flex-col gap-4 p-5 rounded-xl shadow-lg bg-slate-100">
-        <div>
-          <h1 className="text-3xl font-semibold">Bienvenido</h1>
-          <p className="text-lg">Iniciar sesión para continuar</p>
-        </div>
-        
-        <div className="flex flex-col gap-4 text-md">
-            <label>Email</label>
-            <input type="email" className="border-2 p-1" placeholder="name@example.com" />
-
-            <label>Contraseña</label>
-            <input type="password" className="border-2 p-1" placeholder="*****" />
-
-          <div className="flex gap-1">
-            <input type="checkbox" className="border-2" value="remember-me" />
-            <label className="form-check-label">Recuérdame</label>
+    <main className="grid justify-center gap-4 p-5 py-9">
+      <form className="flex flex-col gap-4 p-5 shadow-lg border border-slate-900 sm:w-72 md:w-80">
+        <section className="grid gap-4">
+          <div>
+            <h1 className="text-3xl font-semibold">Bienvenido</h1>
+            <p className="text-lg font-extralight">
+              Iniciar sesión para continuar
+            </p>
           </div>
-        </div>
 
-        <button className="border-2 p-1 text-white bg-slate-900" type="submit">
-          Ingresar
-        </button>
+          <div className="flex flex-col gap-4 text-md">
+            <input
+              type="email"
+              className="font-thin border border-slate-900 p-1"
+              placeholder="username or email"
+            />
+
+            <input
+              type="password"
+              className="font-thin border border-slate-900 p-1"
+              placeholder="contraseña"
+            />
+
+            <button
+              className="text-white bg-gray-800 hover:bg-gray-900 font-medium text-sm p-2  dark:hover:bg-gray-700"
+              type="submit"
+            >
+              Ingresar
+            </button>
+
+            <div className="flex justify-center">
+              <p className="font-extralight">¿Olvidaste la contraseña?</p>
+            </div>
+
+            <div className="">
+              <article className="flex gap-1 items-center justify-between font-extralight">
+                <div className="w-20 border-t border-gray-400 md:w-24 lg:w-28"></div>
+                <div>o</div>
+                <div className="w-20 border-t border-gray-400 md:w-24 lg:w-28"></div>
+              </article>
+
+              <article className="flex flex-col items-center font-extralight">
+                <div className="flex items-center gap-1">
+                  <FcGoogle />
+                  Ingresa con Google
+                </div>
+
+                <div className="flex items-center gap-1">
+                  <AiFillFacebook className="text-blue-700" />
+                  Ingresa con Facebook
+                </div>
+              </article>
+            </div>
+          </div>
+        </section>
       </form>
+
+      <section className="font-extralight grid p-5 shadow-lg border border-slate-900 sm:flex sm:gap-1 sm:w-72 md:w-80">
+        <h2>¿No tienes una cuenta?</h2>
+        <h2 className="text-sky-500">Registrate</h2>
+      </section>
     </main>
   );
 }
-
