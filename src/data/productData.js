dac9bf52b64a70e79cfe251669bd7aeb0ad09632
henryhyde11 <serestrepov@gmail.com>
2023-08-06 13:26:19 -0500

    Women images added

---
+++ src/data/productData.js
@@ -0,0 +1,86 @@
+import womenCamisetaNegra from "../data/women-img/mujer-camiseta-negra.jpg";
+import womenCamisetaBlanca from "../data/women-img/mujer-camiseta-blanca.jpg";
+import womenCamisetaRosa from "../data/women-img/mujer-camiseta-rosa.jpg";
+import womenZapatosNegros from "../data/women-img/mujer-zapatos-negros.jpg";
+import womenZapatosBlancos from "../data/women-img/mujer-zapatos-blancos.jpg";
+import womenZapatosRosa from "../data/women-img/mujer-zapatos-rosa.jpg";
+import womenSombreroNegro from "../data/women-img/mujer-sombrero-negro.jpg";
+import womenSombreroBlanco from "../data/women-img/mujer-sombrero-blanco.jpg";
+import womenSombreroRosa from "../data/women-img/mujer-sombrero-rosa.jpg";
+
+export const products = {
+  women: [
+    {
+      id: "0",
+      title: "Camiseta negra",
+      image: womenCamisetaNegra,
+      price: "200",
+      category: "camiseta",
+      discount: "4 cuotas de $50",
+    },
+    {
+      id: "1",
+      title: "Camiseta blanca",
+      image: womenCamisetaBlanca,
+      price: "200",
+      discount: "4 cuotas de $50",
+      category: "camiseta",
+    },
+    {
+      id: "2",
+      title: "Camiseta rosa",
+      image: womenCamisetaRosa,
+      price: "200",
+      discount: "4 cuotas de $50",
+      category: "camiseta",
+    },
+    {
+      id: "3",
+      title: "Zapatos negros",
+      image: womenZapatosNegros,
+      price: "250",
+      discount: "4 cuotas de $62.5",
+      category: "zapatos",
+    },
+    {
+      id: "4",
+      title: "Zapatos blancos",
+      image: womenZapatosBlancos,
+      price: "250",
+      discount: "4 cuotas de $62.5",
+      category: "zapatos",
+    },
+    {
+      id: "5",
+      title: "Zapatos rosa",
+      image: womenZapatosRosa,
+      price: "250",
+      discount: "4 cuotas de $62.5",
+      category: "zapatos",
+    },
+    {
+      id: "6",
+      title: "Sombrero negro",
+      image: womenSombreroNegro,
+      price: "100",
+      discount: "4 cuotas de $25",
+      category: "sombrero",
+    },
+    {
+      id: "7",
+      title: "Sombrero blanco",
+      image: womenSombreroBlanco,
+      price: "100",
+      discount: "4 cuotas de $25",
+      category: "sombrero",
+    },
+    {
+      id: "8",
+      title: "Sombrero rosa",
+      image: womenSombreroRosa,
+      price: "100",
+      discount: "4 cuotas de $25",
+      category: "sombrero",
+    },
+  ],
+};
