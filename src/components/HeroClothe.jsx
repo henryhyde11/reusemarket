fab6e387567d5c5a04001f47de103cf78137b924
henryhyde11 <serestrepov@gmail.com>
2023-08-05 22:00:20 -0500

    Filter modified

--- src/components/HeroClothe.jsx
+++ src/components/HeroClothe.jsx
@@ -3,7 +3,6 @@ import { Link } from "react-router-dom";
 import { GlobalContext } from "../context/GlobalContext";
 import { useContext } from "react";
 
-
 export function HeroClothe() {
   const location = useLocation();
 
@@ -42,43 +41,26 @@ export function HeroClothe() {
         <p className="text-md font-thin sm:text-2xl">{pTitle()}</p>
       </main>
 
-      {/* <!-- SLIDEBAR MOBILE--> */}
+      {/* SLIDEBAR */}
 
-      <aside className="grid gap-2 p-4 sm:hidden">
-        <h1 className="text-2xl font-bold">Filtros</h1>
-
-        <div className="grid grid-rows-1 gap-3 justify-center">
-          <article className="grid grid-cols-3 gap-3">
-            <BtnFilter>Accesorios</BtnFilter>
-            <BtnFilter>Buzos</BtnFilter>
-            <BtnFilter>Camisas</BtnFilter>
-          </article>
-
-          <article className="grid grid-cols-3 gap-3">
-            <BtnFilter>Chaquetas</BtnFilter>
-            <BtnFilter>Medias</BtnFilter>
-            <BtnFilter>Jeans</BtnFilter>
-          </article>
-        </div>
-      </aside>
-
-      {/* SLIDEBAR DESKTOP  */}
-
-      <div className="sm:flex sm:mt-10">
-        <aside className="hidden sm:h-screen sm:flex sm:p-5 sm:sticky sm:top-0">
+      <div className="sm:flex sm:mt-5">
+        <aside className="p-5 sm:h-screen sm:flex sm:sticky sm:top-0">
           <div className="flex flex-col gap-2">
             <article className="flex flex-col">
               <h2 className="text-3xl font-bold">Filtros</h2>
-              <h3 className="text-2xl font-extralight">Categorías</h3>
             </article>
 
-            <article>
-              <BtnCheck>Buzos</BtnCheck>
-              <BtnCheck>Camisas</BtnCheck>
-              <BtnCheck>Chaquetas</BtnCheck>
-              <BtnCheck>Medias</BtnCheck>
-              <BtnCheck>Jeans</BtnCheck>
-              <BtnCheck>Zapatos</BtnCheck>
+            <article className="flex sm:flex-col gap-2">
+              <label htmlFor="category">Categoría</label>
+              <select id="category">
+                <option value="all">Todas</option>
+                <option value="buzo">Buzo</option>
+                <option value="camiseta">Camiseta</option>
+                <option value="sombrero">Sombrero</option>
+                <option value="medias">Medias</option>
+                <option value="jeans">Jeans</option>
+                <option value="zapatos">Zapatos</option>
+              </select>
             </article>
           </div>
         </aside>
@@ -86,35 +68,15 @@ export function HeroClothe() {
         {/* <!-- ALBUM PRODUCTS --> */}
 
         <section className="w-full grid gap-10 p-5 mb-10 justify-center sm:grid-cols-2 lg:grid-cols-3">
-          <CardClothe/>
+          <CardClothe />
         </section>
       </div>
     </>
   );
 }
 
-function BtnFilter({ children }) {
-  return (
-    <button
-      type="button"
-      className="flex justify-center items-center p-1 text-xs border-2 border-gray-600 truncate"
-    >
-      {children}
-    </button>
-  );
-}
-
-function BtnCheck({ children }) {
-  return (
-    <div className="flex gap-2">
-      <input className="" type="checkbox" value="" id="" />
-      <label className="">{children}</label>
-    </div>
-  );
-}
-
 function CardClothe() {
-  const { initialState } = useContext(GlobalContext)
+  const { initialState } = useContext(GlobalContext);
 
   return initialState.map((element) => (
     <article
@@ -122,12 +84,12 @@ function CardClothe() {
       className="grid gap-2 rounded-lg shadow-lg cursor-pointer hover:shadow-sky-500 max-sm:max-w-xs"
     >
       <Link to={"/detail"}>
-      <img className="" src={element.image} alt={element.title} />
+        <img className="" src={element.image} alt={element.title} />
 
-      <div className="grid gap-1 p-2 text-lg">
-        <p className="font-semibold">{element.title}</p>
-        <p className="">$ {element.price}</p>
-      </div>
+        <div className="grid gap-1 p-2 text-lg">
+          <p className="font-semibold">{element.title}</p>
+          <p className="">$ {element.price}</p>
+        </div>
       </Link>
     </article>
   ));
