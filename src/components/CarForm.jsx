b01803b4816c990f6fd10fc9461a67dd4751dd44
henryhyde11 <serestrepov@gmail.com>
2023-08-10 17:01:59 -0500

    CarForm modified, cart empty render when no elements added

--- src/components/CarForm.jsx
+++ src/components/CarForm.jsx
@@ -1,16 +1,14 @@
 import { useContext } from "react";
 import { CarListContext } from "../context/CarList";
 
+import { BsFillCartXFill } from "react-icons/bs";
+
 export function CarForm() {
   const { cart, totalPrice } = useContext(CarListContext);
 
   function showProducts() {
     if (cart.length === 0) {
-      return (
-        <div className="p-2 text-xl md:text-2xl text-white text-center bg-sky-500">
-          <h1>Añade elementos al carrito</h1>
-        </div>
-      );
+      return "";
     } else {
       return cart.map((element) => (
         <li key={element.id} className="p-3 border border-gray-900">
@@ -33,139 +31,162 @@ export function CarForm() {
   return (
     <>
       <main className="w-full grid">
-        <div className="w-full grid gap-6 p-5 justify-items-center lg:grid-cols-2">
-          <section className="w-full grid gap-5 sm:max-w-md lg:order-2 lg:h-fit">
-            <div className="flex items-center justify-between lg:">
-              <h4 className="text-2xl">Tu carrito</h4>
-              <span className="px-2 text-white rounded-full bg-sky-500">
-                {cart.length}
-              </span>
+        {cart.length === 0 ? (
+          <>
+            <div className="p-6 flex flex-col gap-4 items-center text-black">
+              <h1 className="text-4xl">
+                Carrito vacío: añade elementos al carrito
+              </h1>
+              <BsFillCartXFill className="w-60 h-60" />
             </div>
-
-            <ul className="grid gap-4">{showProducts()}</ul>
-
-            {cart == 0 ? (
-              ""
-            ) : (
-              <div className="p-2 flex flex-col gap-2 border border-gray-900">
-                <div className="flex justify-between">
-                  <h5 className="text-xl font-medium">Total a pagar</h5>
-                  <h5 className="text-xl font-medium">${totalPrice(cart)}</h5>
+          </>
+        ) : (
+          <>
+            <div className="w-full grid gap-6 p-5 justify-items-center lg:grid-cols-2">
+              <section className="w-full grid gap-5 sm:max-w-md lg:order-2 lg:h-fit">
+                <div className="flex items-center justify-between lg:">
+                  <h4 className="text-2xl">Tu carrito</h4>
+                  <span className="px-2 text-white rounded-full bg-sky-500">
+                    {cart.length}
+                  </span>
                 </div>
-              </div>
-            )}
-
-            <form>
-              <div className="grid grid-cols-2 gap-4">
-                <input
-                  className="p-1 border border-slate-900"
-                  type="text"
-                  placeholder="Código"
-                />
-                <button
-                  className="text-white bg-gray-800 hover:bg-gray-900 font-medium text-sm p-2  dark:hover:bg-gray-700"
-                  type="submit"
-                >
-                  Redimir
-                </button>
-              </div>
-            </form>
-          </section>
-
-          <section className="w-full grid gap-4 sm:max-w-md lg:w-full lg:h-fit">
-            <h2 className="text-2xl font-normal">
-              Información de entrega
-            </h2>
-            <form className="grid gap-4">
-              <div className="grid gap-4">
-                <FormInput type={"text"} placeholder={"Nombres"} />
-                <FormInput type={"text"} placeholder={"Apellidos"} />
-                <FormInput type={"email"} placeholder={"Email"} />
-                <FormInput type={"text"} placeholder={"Dirección"} />
-
-                <div className="max-sm:grid max-sm:gap-4 sm:flex sm:justify-between">
-                  <div className="flex flex-col gap-1 text-md">
-                    <select className="sm:w-24 p-1 border border-slate-900">
-                      <option hidden>País</option>
-                      <option>Colombia</option>
-                      <option>Panamá</option>
-                      <option>México</option>
-                    </select>
+                <ul className="grid gap-4">{showProducts()}</ul>
+                <div className="p-2 flex flex-col gap-2 border border-gray-900">
+                  <div className="flex justify-between">
+                    <h5 className="text-xl font-medium">Total a pagar</h5>
+                    <h5 className="text-xl font-medium">${totalPrice(cart)}</h5>
                   </div>
-
-                  <div className="flex flex-col text-md">
-                    <select
-                      className="sm:w-24 p-1 border border-slate-900"
-                      placeholder="ciudad"
-                    >
-                      <option hidden>Ciudad</option>
-                      <option>Medellín</option>
-                      <option>Bello</option>
-                      <option>París</option>
-                    </select>
-                  </div>
-
-                  <div className="flex flex-col text-md">
+                </div>
+                <form>
+                  <div className="grid grid-cols-2 gap-4">
                     <input
-                      className="p-1 sm:w-28 border border-slate-900"
+                      className="p-1 border border-slate-900"
                       type="text"
-                      placeholder="Código postal"
+                      placeholder="Código del bono"
+                    />
+                    <button
+                      className="text-white bg-gray-800 hover:bg-gray-900 font-medium text-sm p-2  dark:hover:bg-gray-700"
+                      type="submit"
+                    >
+                      Redimir
+                    </button>
+                  </div>
+                </form>
+              </section>
+              <section className="w-full grid gap-4 sm:max-w-md lg:w-full lg:h-fit">
+                <h2 className="text-2xl font-normal">Información de entrega</h2>
+
+                <form className="grid gap-4">
+                  <div className="grid gap-4">
+                    <input
+                      className="p-1 border border-slate-900"
+                      placeholder="Nombres"
+                      type="text"
+                    />
+                    <input
+                      className="p-1 border border-slate-900"
+                      placeholder="Apellidos"
+                      type=""
+                    />
+                    <input
+                      className="p-1 border border-slate-900"
+                      placeholder="Email"
+                    />
+                    <input
+                      className="p-1 border border-slate-900"
+                      placeholder="Dirección"
+                    />
+
+                    <div className="max-sm:grid max-sm:gap-4 sm:flex sm:justify-between">
+                      <div className="flex flex-col gap-1 text-md">
+                        <select className="sm:w-24 p-1 border border-slate-900">
+                          <option hidden>País</option>
+                          <option>Colombia</option>
+                          <option>Panamá</option>
+                          <option>México</option>
+                        </select>
+                      </div>
+
+                      <div className="flex flex-col text-md">
+                        <select
+                          className="sm:w-24 p-1 border border-slate-900"
+                          placeholder="ciudad"
+                        >
+                          <option hidden>Ciudad</option>
+                          <option>Medellín</option>
+                          <option>Bello</option>
+                          <option>París</option>
+                        </select>
+                      </div>
+
+                      <div className="flex flex-col text-md">
+                        <input
+                          className="p-1 sm:w-28 border border-slate-900"
+                          type="text"
+                          placeholder="Código postal"
+                        />
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="grid gap-2">
+                    <h4 className="text-2xl font-normal">
+                      Información de pago
+                    </h4>
+
+                    <div className="flex flex-col">
+                      <div className="flex gap-2">
+                        <input type="radio" />
+                        <label className="text-lg">Tarjeta de crédito</label>
+                      </div>
+
+                      <div className="flex gap-2">
+                        <input type="radio" />
+                        <label className="text-lg">Tarjeta de débito</label>
+                      </div>
+
+                      <div className="flex gap-2">
+                        <input type="radio" />
+                        <label className="text-lg">Transferencia</label>
+                      </div>
+                    </div>
+                  </div>
+
+                  <div className="grid gap-4">
+                    <input
+                      className="p-1 border border-slate-900"
+                      placeholder="Nombre en la tarjeta"
+                      type="text"
+                    />
+                    <input
+                      className="p-1 border border-slate-900"
+                      placeholder="Número de la tarjeta"
+                      type="text"
+                    />
+                    <input
+                      className="p-1 border border-slate-900"
+                      placeholder="Fecha de expedición"
+                      type="text"
+                    />
+                    <input
+                      className="p-1 border border-slate-900"
+                      placeholder="CVC"
+                      type="text"
                     />
                   </div>
-                </div>
-              </div>
 
-              <div className="grid gap-2">
-                <h4 className="text-2xl font-normal">Información de pago</h4>
-                <div>
-                  <FormInputRadio textLabel={"Tarjeta de crédito"} />
-                  <FormInputRadio textLabel={"Tarjeta de débito"} />
-                  <FormInputRadio textLabel={"Transferencia"} />
-                </div>
-              </div>
-
-              <div className="grid gap-4">
-                <FormInput type={"text"} placeholder={"Nombre en la tarjeta"} />
-                <FormInput type={"text"} placeholder={"Número de la tarjeta"} />
-                <FormInput type={"text"} placeholder={"Fecha de expedición"} />
-                <FormInput type={"text"} placeholder={"CVC"} />
-              </div>
-
-              <button
-                className="text-white bg-gray-800 hover:bg-gray-900 font-medium text-sm p-2  dark:hover:bg-gray-700"
-                type="submit"
-              >
-                Finalizar compra
-              </button>
-            </form>
-          </section>
-        </div>
+                  <button
+                    className="text-white bg-gray-800 hover:bg-gray-900 font-medium text-sm p-2  dark:hover:bg-gray-700"
+                    type="submit"
+                  >
+                    Finalizar compra
+                  </button>
+                </form>
+              </section>
+            </div>
+          </>
+        )}
       </main>
-    </>
-  );
-}
-
-function FormInput({ type, placeholder }) {
-  return (
-    <>
-      <div className="flex flex-col text-md xl:w-full">
-        <input
-          className="p-1 border border-slate-900"
-          type={type}
-          placeholder={placeholder}
-        />
-      </div>
-    </>
-  );
-}
-
-function FormInputRadio({ textLabel }) {
-  return (
-    <>
-      <div className="flex gap-1">
-        <input type="radio" />
-        <label className="text-lg">{textLabel}</label>
-      </div>
     </>
   );
 }
